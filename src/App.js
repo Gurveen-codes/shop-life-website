973a76d1d8252381b0bf6ca425279f8e80c4a867
Gurveen <gurveens11@gmail.com>
2021-02-24 14:15:23 +0530

    Implement Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./navbar.module.css";
+
+const NavBar = () => {
+  return (
+    <div class={classes.mainNav}>
+      <ul class={classes.nav}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Press Kit</a>
+        </li>
+        <li>
+          <a href="#">Seller</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
