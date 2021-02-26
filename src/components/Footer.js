bffd987c6917b67c8af8c0e1604446454b4fe480
Gurveen <gurveens11@gmail.com>
2021-02-26 12:17:08 +0530

    Add Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,19 @@ import classes from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <section>
+    <section className={classes.footerArea}>
       <div className={classes.footerContainer}>
-        <h4>Downloads</h4>
+        <h3>Downloads:</h3>
+        <a href="#">Get Tvish for Android</a>
+        <a href="#">Get Tvish for iOS</a>
+
+        <h3>Follow Us:</h3>
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+
+        <h3>Contact Us:</h3>
+        <a href="#">hello@test.com</a>
+        <a href="#">+91 665544332</a>
       </div>
     </section>
   );
