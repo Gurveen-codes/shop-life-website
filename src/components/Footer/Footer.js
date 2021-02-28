4091b3cba9e99ea9f09df900f5c6cce91cefb150
Gurveen <gurveens11@gmail.com>
2021-02-28 22:48:49 +0530

    re-design footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,74 @@
 import React from "react";
-import classes from "./footer.module.css";
+import classNamees from "./footer.module.css";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <section className={classes.footerArea}>
-      <div className={classes.footerContainer}>
-        <h3>Downloads:</h3>
-        <a href="#">Get Tvish for Android</a>
-        <a href="#">Get Tvish for iOS</a>
+    <section className="footerArea">
+      <footer className="footer">
+        <div className="footer_social">
+          <h1 className="footer_logo">Tvish</h1>
+          <div className="icons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Instagram</a>
+          </div>
+        </div>
 
-        <h3>Follow Us:</h3>
-        <a href="#">Facebook</a>
-        <a href="#">Instagram</a>
+        <ul className="footer_nav">
+          <li className="nav_item">
+            <h2 className="nav_title">Download</h2>
 
-        <h3>Contact Us:</h3>
-        <a href="#">hello@test.com</a>
-        <a href="#">+91 665544332</a>
-      </div>
+            <ul className="nav_ul">
+              <li>
+                <a href="#">Get Tvish for Android</a>
+              </li>
+
+              <li>
+                <a href="#">Get Tvish for iOS</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav_item ">
+            <h2 className="nav_title">Resources</h2>
+
+            <ul className="nav_ul ">
+              <li>
+                <a href="#">Homepage</a>
+              </li>
+              <li>
+                <a href="#">Seller Portal</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav_item">
+            <h2 className="nav_title">Legal</h2>
+
+            <ul className="nav_ul">
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+
+              <li>
+                <a href="#">Terms of Use</a>
+              </li>
+
+              <li>
+                <a href="#">Sitemap</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <div className="legal">
+          <p>&copy; 2021 Tvish. All rights reserved.</p>
+        </div>
+      </footer>
     </section>
   );
 };
